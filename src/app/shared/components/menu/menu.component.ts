0f99a77ea74887c042983f784bbe60b2fbdb4496
jheisson  rojas <jfrojas592@misena.edu.co>
2024-11-08 08:03:59 -0500

    o

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -6,34 +6,36 @@ import { MenuItem } from 'primeng/api';
   templateUrl: './menu.component.html',
   styles: ``,
 })
+
 export class MenuComponent implements OnInit {
-  public menuItems: MenuItem[] = [];
+
+  menuItems: MenuItem[] = [];
 
   ngOnInit() {
     this.menuItems = [
-    {
-      label: 'pipes de angular ',
-      icon: 'pi pi-desktop',
-      item: [
-        {
-          label: ' Textos y Fechas',
-          icon: ' pi pi-align-left ',
-        },
-        {
-          label: ' Numeros ',
-          icon: ' pi pi-dollar',
-        },
-        {
-          label: ' Np comunes  ',
-          icon: ' pi pi-glove',
-        },
-      ]
-    },
-    {
-      label: ' Pipes Personalizados',
-      icon: ' pi pi-cog'
-    }
-  ];
+      {
+        label: 'pipes de angular ',
+        icon: 'pi pi-desktop',
+        item: [
+          {
+            label: 'Textos y Fechas',
+            icon: 'pi pi-align-left',
+          },
+          {
+            label: 'Numeros',
+            icon: 'pi pi-dollar',
+          },
+          {
+            label: 'No comunes ',
+            icon: 'pi pi-glove',
+          },
+        ]
+      },
+      {
+        label: ' Pipes Personalizados',
+        icon: ' pi pi-cog',
+      }
+    ];
 
   }
 }
