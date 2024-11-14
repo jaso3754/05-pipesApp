120f36a4a4b140dde9c570dd47e21781ce23eeba
jheisson  rojas <jfrojas592@misena.edu.co>
2024-11-14 14:28:19 -0500

    WORK

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './basics-page.component.css'
 })
 export class BasicsPageComponent {
+  public nameLower:string = 'jheison';
+  public nameUpper:string = 'jheison';
+  public fullName:string = 'jHeIsSON fErNAndo';
 
 }
